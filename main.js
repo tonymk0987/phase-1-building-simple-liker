e7cdfb8a9f9d03c309d297ff20a1004928f63b2b
tonymk0987 <k.tony.muriuki@gmail.com>
2024-04-08 14:38:28 +0300

    Completed lab

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+document.addEventListener('DOMContentLoaded', function() {
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
+  const hearts = document.querySelectorAll('.like-glyph');
+  hearts.forEach(heart => {
+    heart.addEventListener('click', function() {
+      if (!heart.classList.contains('activated-heart')) {
+        mimicServerCall()
+          .then(() => {
+            heart.classList.add('activated-heart');
+            heart.textContent = FULL_HEART;
+          })
+          .catch(error => {
+            errorModal.textContent = error;
+            errorModal.classList.remove('hidden');
+            setTimeout(() => {
+              errorModal.classList.add('hidden');
+            }, 3000);
+          });
+      } else {
+        heart.classList.remove('activated-heart');
+        heart.textContent = EMPTY_HEART;
+      }
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
